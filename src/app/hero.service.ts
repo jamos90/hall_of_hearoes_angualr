40c7a555042aadfecfa74d269374a895c19a6985
jamos90 <jamos_98@hotmail.com>
2018-09-02 16:47:35 +0100

    app half way complete

---
+++ src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {hero} from './hero';
+import {HEROES} from './mock-heroes';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Observable<Hero[]>{
+    return of(HEROES);
+  }
+
+}
